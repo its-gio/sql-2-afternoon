5f9b150c968c4144056b6559b78b6453591f8e03
Giovani Aguirre <me@its-g.io>
2020-02-12 15:15:02 -0600

    Updating Rows section compelete.

--- index.sql
+++ index.sql
@@ -98,6 +98,31 @@ WHERE name = 'Queen'
   )
 
 -- Practice updating Rows
+UPDATE customer
+SET fax = null
+
+UPDATE customer
+SET company = 'Self'
+WHERE company IS null
+
+UPDATE customer
+SET last_name = 'Thompson'
+WHERE first_name = 'Julia' AND last_name = 'Barnett'
+
+UPDATE customer
+SET support_rep_id = 4
+WHERE email = 'luisrojas@yahoo.cl'
+
+UPDATE track
+SET composer = 'The darkness around us'
+WHERE composer IS null AND genre_id IN (
+	SELECT genre_id
+  FROM genre
+  WHERE name = 'Metal'
+)
+
+
+
 -- Group by
 -- Use Distinct
 -- Delete Rows
